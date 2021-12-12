e7fb0645aa5287e41afe68c3f4637688f943a275
Stefan-Vicentiu <94748045+Stefan-Vicentiu@users.noreply.github.com>
2021-12-12 14:00:58 +0200

    Update questions.sql

--- SQL Deep Dive/Having/questions.sql
+++ SQL Deep Dive/Having/questions.sql
@@ -4,6 +4,12 @@
 *  Database: Employees
 */
 
+SELECT e.emp_no, CONCAT(e.first_name, ' ', e.last_name) AS "Full Name", COUNT(t.title)
+FROM employees AS e
+JOIN titles AS t ON e.emp_no = t.emp_no
+WHERE EXTRACT(YEAR FROM e.hire_date) > 1991
+GROUP BY e.emp_no, e.first_name, e.last_name
+HAVING COUNT(t.title) > 2;
 
 
 /*
@@ -11,6 +17,14 @@
 *  Database: Employees
 */
 
+SELECT e.emp_no, CONCAT(e.first_name, ' ', e.last_name) AS "Full Name", COUNT(s.salary) AS "Salary Changes"
+FROM employees AS e
+JOIN salaries AS s ON e.emp_no = s.emp_no
+JOIN dept_emp AS de ON de.emp_no = e.emp_no
+JOIN departments AS d ON d.dept_no = de.dept_no
+GROUP BY e.emp_no, CONCAT(e.first_name, ' ', e.last_name), d.dept_name
+HAVING d.dept_name ILIKE 'development' AND COUNT(s.salary) > 15
+ORDER BY COUNT(s.salary) DESC;
 
 
 /*
@@ -18,3 +32,11 @@
 *  Database: Employees
 */
 
+SELECT e.emp_no, CONCAT(e.first_name, ' ', e.last_name) AS "Full Name", COUNT(d.dept_name) AS "Count in departments"
+FROM employees AS e
+JOIN salaries AS s ON e.emp_no = s.emp_no
+JOIN dept_emp AS de ON de.emp_no = e.emp_no
+JOIN departments AS d ON d.dept_no = de.dept_no
+GROUP BY e.emp_no
+HAVING COUNT(d.dept_name) > 1
+ORDER BY COUNT(d.dept_name) DESC;
